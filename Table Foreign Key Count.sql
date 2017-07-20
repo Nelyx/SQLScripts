0301adfd2933f877ab9ff53196c9a14d9d172f55
Rohan Smith <rohan.smith@1ict.com.au>
2017-07-20 10:40:55 +1000

    Allowed tables with 0 foreign keys. And added a bogus ratio (as a rough guide for dependence)

--- Table Foreign Key Count.sql
+++ Table Foreign Key Count.sql
@@ -3,16 +3,18 @@
 select
     [s].[name]+'.'+[t].[Name] as [ObjectName],
     count(distinct [fkin].object_id) as [FkIn],
-    count(distinct [fkOut].object_id) as [FkOut]
+    count(distinct [fkOut].object_id) as [FkOut],
+    (count(distinct [fkin].object_id) + 1.0)
+    / (count(distinct [fkOut].object_id) + 1.0) as [FkBogoRatio]
 from sys.tables as t
     join sys.schemas as s
     on t.schema_id = s.schema_id
-    join sys.foreign_keys as fkIn
+    left join sys.foreign_keys as fkIn
     on t.object_id = fkIn.referenced_object_id
-    join sys.foreign_keys as fkOut
+    left join sys.foreign_keys as fkOut
     on t.object_id = fkOut.Parent_object_Id
 group by
          [t].[Name],
          [s].[name]
-
-order by fkin desc
+order by
+          [FkBogoRatio] desc;
